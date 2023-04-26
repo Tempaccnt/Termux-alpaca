ce84d582a5d772543e73f4e0a54714b7f2edf67d
Tempaccnt <92961973+Tempaccnt@users.noreply.github.com>
2023-04-26 18:03:09 +0300

    Update install-alpaca-mega.sh

--- install-alpaca-mega.sh
+++ install-alpaca-mega.sh
@@ -2,7 +2,7 @@ cd
 # update packages
 pkg update && pkg upgrade -y
 # install necessary packages
-pkg install clang wget git cmake
+pkg install clang wget git cmake megatools
 #cloning the github repository of alpaca.cpp
 git clone  https://github.com/antimatter15/alpaca.cpp
 #enter alpaca.cpp folder
@@ -10,7 +10,7 @@ cd alpaca.cpp
 #start building alpaca.cpp (if you get an error try replqcing make with cmake and see if that works)
 make
 #downloading the 7B alpaca model
-wget https://huggingface.co/Sosaka/Alpaca-native-4bit-ggml/resolve/main/ggml-alpaca-7b-q4.bin
+megadl https://mega.nz/file/3ZI2lA5Y#bAPt0tKWexZMf0GhoI_RMQ0OH4vXWFyfFbykXrIKZAY
 # making a shortcut script to launch alpaca easily with the word chat
 cd /$PREFIX/bin
 echo 'clear' > chat
