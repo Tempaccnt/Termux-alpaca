de91fbac8677736d1c1c006e8c21c0d3859e2b01
Tempaccnt <92961973+Tempaccnt@users.noreply.github.com>
2023-04-27 10:31:35 +0300

    Update install-alpaca-Android.sh

--- install-alpaca-Android.sh
+++ install-alpaca-Android.sh
@@ -1,16 +1,20 @@
 cd
 # update packages
 pkg update && pkg upgrade -y
-# install necessary packages
-pkg install clang wget git cmake -y
+# install necessary packages                                                            pkg install clang wget git megatools cmake -y
 #cloning the github repository of alpaca.cpp
-git clone  https://github.com/antimatter15/alpaca.cpp
-#enter alpaca.cpp folder
-cd alpaca.cpp
-#start building alpaca.cpp (if you get an error try replqcing make with cmake and see if that works)
-make
+git clone  https://github.com/antimatter15/alpaca.cpp                                   #enter alpaca.cpp folder                                                                cd alpaca.cpp
+#start building alpaca.cpp (if you get an error try replqcing make with cmake and see i>
+make                                                                                    echo -e  "\e[32m select whether to use hugginface or mega(recommended) to download the >
+echo -e "enter 1 for huggingface or 2 for mega\e[39m"
+read website
 #downloading the 7B alpaca model
-wget https://huggingface.co/Sosaka/Alpaca-native-4bit-ggml/resolve/main/ggml-alpaca-7b-q4.bin
+if [ $website -eq "1" ]
+        then
+                wget https://huggingface.co/Sosaka/Alpaca-native-4bit-ggml/resolve/main>
+        else
+                megadl https://mega.nz/file/3ZI2lA5Y#bAPt0tKWexZMf0GhoI_RMQ0OH4vXWFyfFb>
+fi
 # making a shortcut script to launch alpaca easily with the word chat
 cd /$PREFIX/bin
 echo 'clear' > chat
